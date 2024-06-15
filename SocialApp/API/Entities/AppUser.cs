2cfbc1a80def5540c02dbf6a38ad55436a5ee671
Sheeza-Zulfiqar <Administrator@LAP-WFH011>
2024-06-15 22:12:48 +0500

    Implemented  Basic Authentication

--- SocialApp/API/Entities/AppUser.cs
+++ SocialApp/API/Entities/AppUser.cs
@@ -2,6 +2,9 @@
 
 public class AppUser
 {
-    public int Id { get; set; }                   
+    public int Id { get; set; }       
+                
     public string UserName { get; set;}
+    public Byte[] PasswordHash { get; set; }
+    public Byte[] PasswordSalt { get; set; }
 }
