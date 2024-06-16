853cbff679aac67eb2219878f27dcaa6a09f0e0e
Sheeza-Zulfiqar <Administrator@LAP-WFH011>
2024-06-16 21:06:19 +0500

    Implemented login and register functionality

--- SocialApp/client/src/app/app.component.ts
+++ SocialApp/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'SocialApp';
-  users: any;
-  constructor(private http: HttpClient) {}
+  title = 'Social App';
+  constructor(private accountService: AccountService) {}
   ngOnInit(): void {
-    this.http.get('http://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
