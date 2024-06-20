920cc4f7ad3f2414e2b958411f3a8c2e608d4896
Sheeza-Zulfiqar <Administrator@LAP-WFH011>
2024-06-20 11:53:27 +0500

    Implemented the user interface

--- SocialApp/client/src/app/app.module.ts
+++ SocialApp/client/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -27,10 +29,10 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     ListsComponent,
     MessagesComponent,
     MemberListComponent,
-    MemberDetailComponent,
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    MemberCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,6 +44,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
